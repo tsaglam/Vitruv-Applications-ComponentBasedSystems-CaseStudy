5d3f58ca8770ab385457a36acb12bafb760d2bb7
Nils Philip <nils.projekt@t-online.de>
2017-12-23 19:31:37 +0100

    Created parameters and data pairs tests.

---
+++ tests/pcmjava/tools.vitruv.applications.pcmjava.confidentialitytransformations.confidentiality2annotations.tests/src/tools/vitruv/applications/pcmjava/tests/confidentialitytransformations/confidentiality2annotations/confidentialityrepository/ParametersAndDataPairsTest.java
@@ -0,0 +1,389 @@
+package tools.vitruv.applications.pcmjava.tests.confidentialitytransformations.confidentiality2annotations.confidentialityrepository;
+
+import java.util.List;
+import java.util.Set;
+
+import org.emftext.language.java.members.EnumConstant;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.kit.kastel.scbs.confidentiality.data.DataSet;
+import edu.kit.kastel.scbs.confidentiality.repository.ParametersAndDataPair;
+import tools.vitruv.applications.pcmjava.tests.confidentialitytransformations.confidentiality2annotations.ConfidentialityApplicationTest;
+import tools.vitruv.applications.pcmjava.tests.confidentialitytransformations.confidentiality2annotations.util.ConfAssertions;
+import tools.vitruv.applications.pcmjava.tests.confidentialitytransformations.confidentiality2annotations.util.ConfElementsCreator;
+
+public class ParametersAndDataPairsTest extends ConfidentialityApplicationTest {
+
+    @Before
+    public void before() {
+
+    }
+
+    @After
+    public void after() {
+
+    }
+
+    @Test
+    public void testAddParametersAndDataPair() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testAddPudPairWithEmptyName() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPairWithName("");
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testAddPudPairWithNullName() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPairWithName(null);
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testRenamePuDPair() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        pudPair.setName(ConfElementsCreator.RENAME);
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testRenamePuDPairWithEmptyName() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        pudPair.setName("");
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testRenamePuDPairWithNullName() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        pudPair.setName(null);
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testAddParameterSource() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        pudPair.getParameterSources().add(ConfElementsCreator.createParameterSource());
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testDeleteParameterSource() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        String parameterSource = ConfElementsCreator.createParameterSource();
+        pudPair.getParameterSources().add(parameterSource);
+        saveAndSynchronizeChanges(getRootElement());
+        pudPair.getParameterSources().remove(parameterSource);
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testChangeParameterSource() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        String parameterSource = ConfElementsCreator.createParameterSource();
+        pudPair.getParameterSources().add(parameterSource);
+        saveAndSynchronizeChanges(getRootElement());
+        final int idx = pudPair.getParameterSources().indexOf(parameterSource);
+        pudPair.getParameterSources().remove(parameterSource);
+        pudPair.getParameterSources().add(idx, ConfElementsCreator.createParameterSource());
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testChangeOneOfMultipleParameterSources() throws Throwable {
+        final List<ParametersAndDataPair> pudPairs = addPuDPairs(3);
+        final ParametersAndDataPair pudPair = pudPairs.get(2);
+
+        final List<String> parameterSources = ConfElementsCreator.createParameterSources(2);
+        for (final String parameterSource : parameterSources) {
+            pudPair.getParameterSources().add(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        final String toChange = ConfElementsCreator.createParameterSource();
+        pudPair.getParameterSources().add(toChange);
+        saveAndSynchronizeChanges(getRootElement());
+
+        final List<String> parameterSources2 = ConfElementsCreator.createParameterSources(2);
+        for (final String parameterSource : parameterSources2) {
+            pudPair.getParameterSources().add(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+
+        final int idx = pudPair.getParameterSources().indexOf(toChange);
+        pudPair.getParameterSources().remove(toChange);
+        pudPair.getParameterSources().add(idx, ConfElementsCreator.createParameterSource());
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+        checkPuDPairs(pudPairs);
+    }
+
+    @Test
+    public void testAddMultipleParameterSources() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        List<String> parameterSources = ConfElementsCreator.createSimpleParameterSources();
+        for (String parameterSource : parameterSources) {
+            pudPair.getParameterSources().add(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testAddAndDeleteMultipleParameterSources() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        List<String> parameterSources = ConfElementsCreator.createSimpleParameterSources();
+        for (String parameterSource : parameterSources) {
+            pudPair.getParameterSources().add(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        for (String parameterSource : parameterSources) {
+            pudPair.getParameterSources().remove(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testAddAndDeleteMultipleParameterSourcesNonMonotone() throws Throwable {
+        final List<ParametersAndDataPair> pudPairs = addPuDPairs(3);
+        final ParametersAndDataPair pudPair = pudPairs.get(2);
+
+        final List<String> parameterSources = ConfElementsCreator.createParameterSources(2);
+        for (final String parameterSource : parameterSources) {
+            pudPair.getParameterSources().add(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        final List<String> toRemove = ConfElementsCreator.createParameterSources(2);
+        for (final String parameterSource : toRemove) {
+            pudPair.getParameterSources().add(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        for (final String parameterSource : toRemove) {
+            pudPair.getParameterSources().remove(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        final List<String> parameterSources2 = ConfElementsCreator.createParameterSources(2);
+        for (final String parameterSource : parameterSources2) {
+            pudPair.getParameterSources().add(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        checkPuDPair(pudPair);
+        checkPuDPairs(pudPairs);
+    }
+
+    @Test
+    public void testAddDataSetArgument() throws Throwable {
+        DataSet dataSet = addDataSet();
+        ParametersAndDataPair pudPair = addPuDPair();
+        pudPair.getDataTargets().add(dataSet);
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testDeleteDataSetArgument() throws Throwable {
+        DataSet dataSet = addDataSet();
+        ParametersAndDataPair pudPair = addPuDPair();
+        pudPair.getDataTargets().add(dataSet);
+        saveAndSynchronizeChanges(getRootElement());
+        pudPair.getDataTargets().remove(dataSet);
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testChangeDataSetArgument() throws Throwable {
+        DataSet dataSet = addDataSet();
+        DataSet dataSet2 = addDataSet();
+        ParametersAndDataPair pudPair = addPuDPair();
+        pudPair.getDataTargets().add(dataSet);
+        saveAndSynchronizeChanges(getRootElement());
+        final int idx = pudPair.getDataTargets().indexOf(dataSet);
+        pudPair.getDataTargets().remove(dataSet);
+        pudPair.getDataTargets().add(idx, dataSet2);
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testChangeOneOfMultipleDataSetArguments() throws Throwable {
+        final List<ParametersAndDataPair> pudPairs = addPuDPairs(3);
+        final ParametersAndDataPair pudPair = pudPairs.get(2);
+
+        final List<DataSet> dataSets = addDataSets(2);
+        for (final DataSet dataSet : dataSets) {
+            pudPair.getDataTargets().add(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        DataSet toChange = addDataSet();
+        DataSet newDataSet = addDataSet();
+        pudPair.getDataTargets().add(toChange);
+        saveAndSynchronizeChanges(getRootElement());
+
+        final List<DataSet> dataSets2 = addDataSets(2);
+        for (final DataSet dataSet : dataSets2) {
+            pudPair.getDataTargets().add(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+
+        final int idx = pudPair.getDataTargets().indexOf(toChange);
+        pudPair.getDataTargets().remove(toChange);
+        pudPair.getDataTargets().add(idx, newDataSet);
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPair(pudPair);
+        checkPuDPairs(pudPairs);
+    }
+
+    @Test
+    public void testAddMultipleDataSetArguments() throws Throwable {
+        final ParametersAndDataPair pudPair = addPuDPair();
+        final List<DataSet> dataSets = addDataSets(5);
+        for (final DataSet dataSet : dataSets) {
+            pudPair.getDataTargets().add(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testAddAndDeleteMultipleDataSetArguments() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        final List<DataSet> dataSets = addDataSets(5);
+        for (DataSet dataSet : dataSets) {
+            pudPair.getDataTargets().add(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        for (DataSet dataSet : dataSets) {
+            pudPair.getDataTargets().remove(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testAddAndDeleteMultipleDataSetArgumentsNonMonotone() throws Throwable {
+        final List<ParametersAndDataPair> pudPairs = addPuDPairs(3);
+        final ParametersAndDataPair pudPair = pudPairs.get(2);
+
+        final List<DataSet> dataSets = addDataSets(2);
+        for (final DataSet dataSet : dataSets) {
+            pudPair.getDataTargets().add(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        final List<DataSet> toRemove = addDataSets(2);
+        for (final DataSet dataSet : toRemove) {
+            pudPair.getDataTargets().add(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        for (final DataSet dataSet : toRemove) {
+            pudPair.getDataTargets().remove(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        final List<DataSet> dataSets2 = addDataSets(2);
+        for (final DataSet dataSet : dataSets2) {
+            pudPair.getDataTargets().add(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        checkPuDPair(pudPair);
+        checkPuDPairs(pudPairs);
+    }
+
+    @Test
+    public void testAddParameterSourcesAndDataSetArguments() throws Throwable {
+        final ParametersAndDataPair pudPair = addPuDPair();
+        final List<DataSet> dataSets = addDataSets(5);
+        for (final DataSet dataSet : dataSets) {
+            pudPair.getDataTargets().add(dataSet);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        List<String> parameterSources = ConfElementsCreator.createComplexParameterSources();
+        for (String parameterSource : parameterSources) {
+            pudPair.getParameterSources().add(parameterSource);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        checkPuDPair(pudPair);
+    }
+
+    @Test
+    public void testDeletePuDPair() throws Throwable {
+        ParametersAndDataPair pudPair = addPuDPair();
+        getRootElement().getParametersAndDataPairs().remove(pudPair);
+        saveAndSynchronizeChanges(getRootElement());
+        ConfAssertions.assertEmptyCorrespondences(pudPair, correspondences().emptyCorrespondences(pudPair));
+    }
+
+    @Test
+    public void testAddMultiplePuDPairs() throws Throwable {
+        List<ParametersAndDataPair> pudPairs = addPuDPairs(3);
+        checkPuDPairs(pudPairs);
+    }
+
+    @Test
+    public void testAddAndDeleteMultiplePuDPairs() throws Throwable {
+        List<ParametersAndDataPair> pudPairs = addPuDPairs(3);
+        for (ParametersAndDataPair pudPair : pudPairs) {
+            getRootElement().getParametersAndDataPairs().remove(pudPair);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        for (final ParametersAndDataPair pudPair : pudPairs) {
+            ConfAssertions.assertEmptyCorrespondences(pudPair, correspondences().emptyCorrespondences(pudPair));
+        }
+    }
+
+    @Test
+    public void testAddAndDeleteMultiplePuDPairsNonMonotone() throws Throwable {
+        final int amount = 5;
+        final int amountToRemove = 3;
+        final int amountToAddLater = 4;
+        // create
+        List<ParametersAndDataPair> pudPairs = addPuDPairs(amount);
+        List<ParametersAndDataPair> toRemove = addPuDPairs(amountToRemove);
+        // delete
+        for (final ParametersAndDataPair pudPair : toRemove) {
+            getRootElement().getParametersAndDataPairs().remove(pudPair);
+            saveAndSynchronizeChanges(getRootElement());
+        }
+        // create again
+        List<ParametersAndDataPair> toAddLater = addPuDPairs(amountToAddLater);
+        checkPuDPairs(pudPairs);
+        checkPuDPairs(toAddLater);
+        for (final ParametersAndDataPair pudPair : toRemove) {
+            ConfAssertions.assertEmptyCorrespondences(pudPair, correspondences().emptyCorrespondences(pudPair));
+        }
+    }
+
+    @Test
+    public void testRenameOneOfMultiplePuDPairs() throws Throwable {
+        List<ParametersAndDataPair> pudPairs = addPuDPairs(3);
+        ParametersAndDataPair toRename = addPuDPair();
+        toRename.setName(ConfElementsCreator.RENAME);
+        saveAndSynchronizeChanges(getRootElement());
+        checkPuDPairs(pudPairs);
+        checkPuDPair(toRename);
+    }
+
+    @Test
+    public void testAddDataSetAndItsArgument() throws Throwable {
+        DataSet dataSet = ConfElementsCreator.createDataSet();
+        getRootElement().getDataIdentifier().add(dataSet);
+        saveAndSynchronizeChanges(getRootElement());
+        ParametersAndDataPair pudPair = addPuDPair();
+        pudPair.getDataTargets().add(dataSet);
+        saveAndSynchronizeChanges(getRootElement());
+        final Set<EnumConstant> corresponding = correspondences().getCorrespondingEnumConstants(dataSet);
+        EnumConstant enumConstant = ConfAssertions.getSingleObject(corresponding);
+        ConfAssertions.assertEqualDataSets(dataSet, enumConstant);
+        checkPuDPair(pudPair);
+    }
+}
--- tests/pcmjava/tools.vitruv.applications.pcmjava.confidentialitytransformations.confidentiality2annotations.tests/src/tools/vitruv/applications/pcmjava/tests/confidentialitytransformations/confidentiality2annotations/util/ConfElementsCreator.xtend
+++ tests/pcmjava/tools.vitruv.applications.pcmjava.confidentialitytransformations.confidentiality2annotations.tests/src/tools/vitruv/applications/pcmjava/tests/confidentialitytransformations/confidentiality2annotations/util/ConfElementsCreator.xtend
@@ -5,8 +5,24 @@ import edu.kit.kastel.scbs.confidentiality.ConfidentialityFactory
 import edu.kit.kastel.scbs.confidentiality.data.DataFactory
 import edu.kit.kastel.scbs.confidentiality.repository.RepositoryFactory
 import java.util.LinkedList
+import org.palladiosimulator.pcm.repository.Repository
+import tools.vitruv.applications.pcmjava.tests.util.Pcm2JavaTestUtils
+import org.palladiosimulator.pcm.repository.OperationInterface
+import org.palladiosimulator.pcm.repository.OperationSignature
 
 public final class ConfElementsCreator {
+	
+	public static val PARAMETER_SOURCE = "testParameterSource"
+	
+	public static val CALL = "\\call"
+	
+	public static val RESULT = "\\result"
+	
+	public static val ARRAY_SUFFIX = "[*]"
+		
+	public static val LENGTH_SUFFIX = "length"
+	
+    public static val RENAME = "rename";
 	
     private static var GLOBAL_COUNTER = 0;
 
@@ -23,7 +39,7 @@ public final class ConfElementsCreator {
 
     public static def createParametersAndDataPair() {
         val created = RepositoryFactory.eINSTANCE.createParametersAndDataPair()
-        // overwrite standard name
+        // overwrite standard id and name
         val id = getId
         created.id = id
         created.name = '''pair_«id»'''
@@ -40,7 +56,7 @@ public final class ConfElementsCreator {
 
     public static def createDataSet() {
         val created = DataFactory.eINSTANCE.createDataSet()
-        // overwrite standard name
+        // overwrite standard id and name
         val id = getId
         created.id = id
         created.name = '''confidential_«id»'''
@@ -53,6 +69,45 @@ public final class ConfElementsCreator {
             dataSets.add(createDataSet);
         }
         return dataSets
+    }
+    
+    public static def String createParameterSource() {
+		'''«PARAMETER_SOURCE»«GLOBAL_COUNTER++»'''
+    }
+    
+    public static def createParameterSources(int amount) {
+	    val params = new LinkedList<String>
+        for (var i = 0; i < amount; i++) {
+            params.add(createParameterSource);
+        }
+        return params
+    }
+    
+    public static def createSimpleParameterSources() {
+		#[CALL, '''«createParameterSource()»''', RESULT, '''«RESULT»«ARRAY_SUFFIX»''', '''«RESULT».«LENGTH_SUFFIX»''', '''«createParameterSource()»''']
+    }
+    
+    public static def createComplexParameterSources() {
+    	val params = createParameterSource()
+		#['''«createParameterSource()».«createParameterSource()»''', CALL, '''«RESULT»«ARRAY_SUFFIX».«params»«ARRAY_SUFFIX»''', '''«RESULT».«params».«LENGTH_SUFFIX»''']
+    }
+    
+    public static def Repository createRepository(String repositoryName) {
+        return Pcm2JavaTestUtils.createRepository(repositoryName)
+    }
+    
+    public static def OperationInterface createOperationInterface(Repository repo, String name) {
+        val OperationInterface opInterface = org.palladiosimulator.pcm.repository.RepositoryFactory.eINSTANCE.createOperationInterface
+        opInterface.setRepository__Interface(repo)
+        opInterface.setEntityName(name)
+        return opInterface
+    }
+    
+    public static def OperationSignature createOperationSignature(OperationInterface opInterface, String name) {
+        val OperationSignature opSig = org.palladiosimulator.pcm.repository.RepositoryFactory.eINSTANCE.createOperationSignature();
+        opSig.setEntityName(name);
+        opSig.setInterface__OperationSignature(opInterface);
+        return opSig;
     }
 
     public static def String getId() {
